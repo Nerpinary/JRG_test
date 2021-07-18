198b0af79f6bf52fdce6730d6ca0c4ed1b15af32
Nerpinary <nerpinary5693@gmail.com>
2021-07-18 16:17:40 +0800

    test

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,3 @@
 export const buyButton = document.querySelector(".content__button_data_buy");
 export const sendButton = document.querySelector(".content__button_data_send");
 export const formElementSubscribe = document.querySelector("#formElementSubscribe");
-export const formElementSend = document.querySelector("formElementSend");
